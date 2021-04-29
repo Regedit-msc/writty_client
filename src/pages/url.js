264302eab386a1704581fd34c1e4589b9b729bff
Regedit-msc <efusanyaae@gmail.com>
2021-04-29 14:39:48 +0100

    restored api

--- src/pages/url.js
+++ src/pages/url.js
@@ -1,2 +1,1 @@
-// export const API_ENDPOINT = "https://live-gists.herokuapp.com"
-export const API_ENDPOINT = "http://localhost:3001"
+export const API_ENDPOINT = "https://live-gists.herokuapp.com"
