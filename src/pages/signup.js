34b01b43f458eb0eee475304712971a943c997ce
Regedit-msc <efusanyaae@gmail.com>
2021-06-30 19:57:34 +0100

    made changes to signup

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,25 +1,77 @@
-import { Link } from "react-router-dom"
+import { Link, withRouter } from "react-router-dom"
 import "../css/signup.css";
 import { themeContext } from "../App";
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import { API_ENDPOINT } from "./url";
 
-const Signup = () => {
+const Signup = ({ history }) => {
     const { theme } = useContext(themeContext);
+
+    // const { setUserToken } = useContext(userContext);
+    const [formState, setFormState] = useState({
+        username: '',
+        password: '',
+        email: ""
+    });
+    function handleChange(e) {
+        switch (e.target.name) {
+            case "username":
+                setFormState({ ...formState, username: e.target.value })
+                break;
+            case "password":
+                setFormState({ ...formState, password: e.target.value })
+                break;
+            case "email":
+                setFormState({ ...formState, email: e.target.value });
+                break;
+            default:
+                break;
+        }
+    }
+    function handleSubmit() {
+        console.log(formState);
+        fetch(`${API_ENDPOINT}/register`, {
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8'
+            },
+            method: "POST",
+            body: JSON.stringify(formState)
+        }).then(res => res.json()
+        ).then(jsonRes => {
+            console.log(jsonRes)
+            if (jsonRes.success) {
+                console.log(jsonRes)
+                // setUserToken(jsonRes.message);
+                // history.push('/dash');
+                // setInfo("You are all set proceed to login 🥳");
+                setTimeout(() => {
+                    history.push("/login")
+                }, 3000)
+
+            } else {
+                // setError(jsonRes.message + "😓");
+                // setTimeout(() => {
+                //     setError(null);
+                // }, 2000)
+            }
+        })
+    }
+
     return (
         <>
 
             <h3 id={theme === "light" ? "form_head_signup_light" : "form_head_signup"}>SIGNUP</h3>
             <div id={theme === "light" ? "form_signup_light" : "form_signup"}>
-                <input type="email" id="Email_signup" name="Email" placeholder="Email"></input>
-                <input type="text" id="Username_signup" name="Username" placeholder="Username" />
-                <input type="password" id="Password_signup" name="Password" placeholder="Password" />
+                <input type="email" id="Email_signup" name="email" placeholder="Email" onChange={handleChange} ></input>
+                <input type="text" id="Username_signup" name="username" placeholder="Username" onChange={handleChange} />
+                <input type="password" id="Password_signup" name="password" placeholder="Password" onChange={handleChange} />
             </div>
             <div id="submit_box">
-                <input id={theme === "light" ? "submit_signup_light" : "submit_signup"} type="submit" value="Submit" />
+                <input id={theme === "light" ? "submit_signup_light" : "submit_signup"} type="button" value="Submit" onClick={handleSubmit} />
                 <Link to="/login" id={theme === "light" ? "login_link_light" : "login_link"}>Have an Account?</Link>
             </div>
         </>
     )
 }
 
-export default Signup;
+export default withRouter(Signup);
