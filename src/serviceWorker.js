121cc975176148c327d94c948de8db725c77230c
Regedit-msc <efusanyaae@gmail.com>
2021-07-16 22:34:36 +0100

    Servic worker fix

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -32,7 +32,7 @@ export function register(config) {
         }
 
         window.addEventListener('load', () => {
-            const swFileName = process.env.NODE_ENV === 'production' ? 'service-worker.js' : 'custom-sw.js'
+            const swFileName = process.env.NODE_ENV === 'development' ? 'custom-sw.js' : 'service-worker.js'
             const swUrl = `${process.env.PUBLIC_URL}/${swFileName}`;
 
             if (isLocalhost) {
