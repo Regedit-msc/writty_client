4b104a448679adbac20b07e293b3c6831995c0b2
Regedit-msc <efusanyaae@gmail.com>
2021-07-20 13:08:53 +0100

    sw update

--- public/sw.js
+++ public/sw.js
@@ -8,6 +8,18 @@ self.addEventListener('push', event => {
         badge: "https://avataaars.io/?avatarStyle=Circle&topType=ShortHairDreads02&accessoriesType=Round&hairColor=SilverGray&facialHairType=MoustacheFancy&facialHairColor=Black&clotheType=ShirtScoopNeck&clotheColor=Gray01&eyeType=EyeRoll&eyebrowType=Default&mouthType=Concerned&skinColor=Tanned",
     }
     event.waitUntil(self.registration.showNotification(title, options));
-})
+});
+self.addEventListener('notificationclick', (event) => {
+    event.waitUntil(
+        self.clients.matchAll().then((clientList) => {
+            if (clientList.length > 0) {
+                clientList[0].navigate('/notifications').then((client) => client.focus());
+                return event.notification.close();
+            }
+            self.clients.openWindow('/notifications');
+            return event.notification.close();
+        })
+    );
+});
 
 
