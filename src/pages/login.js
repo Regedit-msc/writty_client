54777fb6bdc44a6bee9ed891cc91c792ee0ff9b5
elijahthis <emperorelijah007@gmail.com>
2021-09-13 20:32:01 +0100

    New Login Page

--- src/pages/login.js
+++ src/pages/login.js
@@ -9,6 +9,11 @@ import { useTitle } from "../utils/title";
 import GoogleLogin from 'react-google-login';
 import { useSnackbar } from 'notistack';
 import LoginGithub from 'react-login-github';
+import LogoPlaceholder from "../images/logo.png"
+import Google from "../images/google-icon.png"
+import GitHub from "../images/github-icon.png"
+import LoginBackgroundImage from "../images/login-background.png"
+
 const Login = ({ history }) => {
     const { enqueueSnackbar } = useSnackbar();
     useTitle("Login.")
@@ -134,39 +139,78 @@ const Login = ({ history }) => {
                 color="red"
                 text={err}
             /> : ""}
-            <h3 id={theme === "light" ? "form_head_login_light" : "form_head_login"}>LOGIN</h3>
-            <div id={theme === "light" ? "form_login_light" : "form_login"}>
-                <input type="text" id="Username" name="username" placeholder="Username" onChange={handleChange} />
-                <input type="password" id="Password" name="password" placeholder="Password" onChange={handleChange} />
+
+            <div className={theme === "light" ? "login-main_light" : "login-main"}>
+                <img src={LogoPlaceholder} className="logo" alt="Logo Placeholder" />
+
+                <div>
+                    <div id="login_form_box">
+                        <header>
+                            <h3 id={theme === "light" ? "login_form_head_light" : "login_form_head"}>Login</h3>
+                            <hr />
+                        </header>
+                        <div id="login_form">
+                            <input type="text" name="Username" placeholder="Username" />
+                            <input type="password" name="Password" placeholder="Password" />
+                            <input type="submit" value="Login" onClick={handleSubmit} />
+                            <div>
+                                <hr className="or" data-content="OR" />
+                            </div>
+                                <div className="login_options">
+                                <img src={Google} alt="Google Logo" />
+                                <GoogleLogin
+                                    clientId={process.env.REACT_APP_G_CLIENT_ID}
+                                    render={(renderProps) => (
+                                        <button
+                                            onClick={() => {
+                                                renderProps.onClick();
+                                            }}
+                                        >
+                                            Sign in with Google
+                                        </button>
+                                    )}
+                                    buttonText='Login'
+                                    onSuccess={responseGoogle}
+                                    onFailure={responseGoogle}
+                                    isSignedIn={false}
+                                    cookiePolicy={'single_host_origin'}
+                                />
+                                </div>
+
+                                <div className="login_options">
+                                <img src={GitHub} alt="GitHub Logo" />
+                                <LoginGithub clientId={process.env.REACT_APP_GH_CLIENT_ID}
+                                    render={(renderProps) => (
+                                        <button
+                                            onClick={() => {
+                                                renderProps.onClick();
+                                            }}
+                                        >
+                                            Sign in with GitHub
+                                        </button>
+                                    )}
+                                    onSuccess={onSuccess}
+                                    onFailure={onFailure}
+                                />
+                                </div>
+                        </div>
+                        <div id="login_footer">
+                            <div><Link to="#">Forgot Password?</Link></div>
+                            <div><span>Don't have an account?</span> <Link to="/signup">Sign up</Link></div>
+                        </div>
+                    </div>
+                </div>
+                <div className="grid-col-3">
+                    <img src={LoginBackgroundImage} alt="Login Background" />
+                </div>
+
+
+                <div id="submit_box">
+
+
+                </div>
+
             </div>
-            <div id="submit_box">
-                <input type="button" id={theme === "light" ? "submit_login_light" : "submit_login"} onClick={handleSubmit} value="Submit" />
-                <GoogleLogin
-                    clientId={process.env.REACT_APP_G_CLIENT_ID}
-                    render={(renderProps) => (
-                        <button
-                            onClick={() => {
-                                renderProps.onClick();
-                            }}
-                        >
-                            Google
-                        </button>
-                    )}
-                    buttonText='Login'
-                    onSuccess={responseGoogle}
-                    onFailure={responseGoogle}
-                    isSignedIn={false}
-                    cookiePolicy={'single_host_origin'}
-                />
-                <LoginGithub clientId={process.env.REACT_APP_GH_CLIENT_ID}
-                    onSuccess={onSuccess}
-                    onFailure={onFailure}
-
-                />
-                <Link to="/signup" id={theme === "light" ? "signup_link_light" : "signup_link"}>Not yet registered?</Link>
-            </div>
-
-
         </>
     )
 }
