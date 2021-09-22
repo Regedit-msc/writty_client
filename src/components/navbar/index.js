83b2d875a6b1df1dc296303449870f29189cfd11
Regedit-msc <efusanyaae@gmail.com>
2021-09-22 10:33:29 +0100

    few changes

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -17,173 +17,339 @@ import { defaultImage } from "../../utils/defaultProfileImage";
 
 
 const NavBar = () => {
+ 
+ const { setTheTheme, theme } = useContext(themeContext);
+ const location = useLocation();
+ const checkBox1 = useRef();
+ const history = useHistory();
+ const [users, setUsers] = useState(null);
+ const { enqueueSnackbar } = useSnackbar();
+ const onlyInclude = useMemo(
+   () => [
+     "/gists",
+     "/dash",
+     "/settings",
+     "/chat",
+     "/new/gist",
+     "/notification",
+   ],
+   []
+ );
+ const [token, setToken] = useState(null);
+ const [profileImage, setProfileImage] = useState(null);
+ const inputRef = useRef();
+ const [value, setValue] = useState("");
+ useEffect(() => {
+   setToken(localStorage.getItem("user_token"));
+ }, [location]);
 
-    const { theme } = useContext(themeContext);
-    const location = useLocation();
-    const history = useHistory();
-    const [users, setUsers] = useState(null);
-    const { enqueueSnackbar } = useSnackbar();
-    const onlyInclude = useMemo(() => ["/gists", '/dash', '/settings', "/chat", "/new/gist", "/notification"], [])
-    const [token, setToken] = useState(null);
-    const [profileImage, setProfileImage] = useState(null);
-    const inputRef = useRef();
-    const [value, setValue] = useState('')
-    useEffect(() => {
-        setToken(localStorage.getItem("user_token"));
-    }, [location]);
+ // const searchUsers = useCallback(() => searchByWordOrLetters, [searchByWordOrLetters])
+ useEffect(() => {
+   inputRef.current = debounce(searchByWordOrLetters, 700);
+ }, []);
+ function searchByWordOrLetters(v) {
+   fetch(API_ENDPOINT + `/search/users?wol=${v}`, {
+     headers: {
+       "Content-Type": "application/json; charset=UTF-8",
+       Authorization: `Bearer ${localStorage.getItem("user_token")}`,
+     },
+     method: "GET",
+   })
+     .then((res) => res.json())
+     .then((response) => {
+       setUsers(response.message);
+       console.log(response.message);
+     });
+ }
 
+ useEffect(() => {
+   if (onlyInclude.includes(window.location.pathname)) {
+     localStorage.setItem("lastVisited", window.location.pathname);
+   } else {
+     return;
+   }
+ }, [location, onlyInclude]);
+ useEffect(() => {
+   if (!token) return;
+   fetch(`${API_ENDPOINT}/user/name`, {
+     headers: {
+       "Content-Type": "application/json; charset=UTF-8",
+       Authorization: `Bearer ${localStorage.getItem("user_token")}`,
+     },
+     method: "GET",
+   })
+     .then((res) => {
+       if (res.ok) return res.json();
+       return res.json().then((json) => Promise.reject(json));
+     })
+     .then((jsonRes) => {
+       setProfileImage(jsonRes.message.profileImageUrl);
+       localStorage.setItem(
+         "profile_user_pic",
+         jsonRes.message.profileImageUrl
+       );
+       localStorage.setItem("profile_user_name", jsonRes.message.username);
+     })
+     .catch(() => {
+       localStorage.removeItem("user_token");
+       history.replace("/login");
+       enqueueSnackbar("Unable to sync.", {
+         variant: "error",
+       });
+     });
+ }, [token, enqueueSnackbar, history]);
+ const notAllowed = ["/", "/register", "/login", "/otp", "/onboard", "/chat"];
 
-    // const searchUsers = useCallback(() => searchByWordOrLetters, [searchByWordOrLetters])
-    useEffect(() => {
-        inputRef.current = debounce(searchByWordOrLetters, 700);
-    }, []);
-    function searchByWordOrLetters(v) {
-        fetch(API_ENDPOINT + `/search/users?wol=${v}`, {
-            headers: {
-                'Content-Type': 'application/json; charset=UTF-8',
-                'Authorization': `Bearer ${localStorage.getItem("user_token")}`
-            },
-            method: "GET",
-        }).then(res => res.json()).then((response) => {
-            setUsers(response.message);
-            console.log(response.message);
-        });
-    }
+ function logOut() {
+   localStorage.removeItem("user_token");
+   localStorage.setItem("new_user", "notreg");
+   localStorage.removeItem("profile_user_image");
+   history.push("/");
+ }
+ const handleChange = (event) => {
+   setValue(event.target.value);
+   const input = event.target.value;
+   if (event.target.value === "" || input.length < 4) {
+     return;
+   }
+   document.querySelector(".search_suggestions").style.display = "flex";
+   inputRef.current(input);
+ };
+ function handleKeyPress(e) {
+   if (e.key === "Enter") {
+     document.querySelector(".search_suggestions").style.display = "none";
+     setValue("");
+     history.replace(`/search?user=${value}`);
+   }
+ }
+ function handleTheme(e) {
+   if (e.target.checked) {
+     setTheTheme("light");
+     document.body.classList.replace("dark", "light");
+     localStorage.setItem("theme_app", "light");
+   } else {
+     document.body.classList.replace("light", "dark");
+     setTheTheme("dark");
+     localStorage.setItem("theme_app", "dark");
+   }
+ }
+ useEffect(() => {
+   if (
+     document.querySelector(
+       theme === "light" ? ".dropdown-content" : ".dropdown-content_dark"
+     )?.style.display === "none" ||
+     !document.querySelector(
+       theme === "light" ? ".dropdown-content" : ".dropdown-content_dark"
+     )
+   )
+     return;
+   checkBox1.current.checked = theme === "light" ? true : false;
+ }, [theme]);
+ return (
+   <>
+     {notAllowed.includes(location.pathname) ||
+     location.pathname.includes("chat") ||
+     location.pathname.includes("setup") ? (
+       ""
+     ) : (
+       <nav className={theme === "light" ? "navbar" : "navbar_dark"}>
+         <ul>
+           <li>
+             <img className="comments" src={LogoPlaceholder} alt="icon" />
+           </li>
+           <li>
+             <Link
+               to="/gists"
+               className={location.pathname === "/gists" ? "active" : ""}
+             >
+               Public Gists
+             </Link>
+           </li>
+           {token ? (
+             <li>
+               <Link
+                 to="/feed"
+                 className={location.pathname === "/feed" ? "active" : ""}
+               >
+                 Feed
+               </Link>
+             </li>
+           ) : (
+             <></>
+           )}
+         </ul>
+         {token ? (
+           <ul className="search-tab">
+             <li>
+               <input
+                 type="search"
+                 name="search"
+                 id="search-nav"
+                 className="search-c"
+                 placeholder="Search"
+                 autoComplete="off"
+                 onClick={() => {
+                   document.querySelector(".search_suggestions").style.display =
+                     "flex";
+                 }}
+                 onChange={handleChange}
+                 onFocus={() => {
+                   document.querySelector(".search_suggestions").style.display =
+                     "flex";
+                 }}
+                 value={value}
+                 onKeyPress={handleKeyPress}
+               />
+             </li>
 
-    useEffect(() => {
-        if (onlyInclude.includes(window.location.pathname)) {
-            localStorage.setItem("lastVisited", window.location.pathname);
+             <div
+               className="search_suggestions"
+               onMouseLeave={() => {
+                 document.querySelector(".search_suggestions").style.display =
+                   "none";
+               }}
+             >
+               {users
+                 ? users.map((user, index) => {
+                     return (
+                       <div key={index}>
+                         <div
+                           className="search_suggestion"
+                           onClick={() => {
+                             setValue("");
+                             history.replace(`/@/${user?.username}`);
+                             document.querySelector(
+                               ".search_suggestions"
+                             ).style.display = "none";
+                           }}
+                         >
+                           {" "}
+                           <img
+                             alt="search_user"
+                             className="profile-img"
+                             src={user?.profileImageUrl ?? defaultImage}
+                           />{" "}
+                           <span>{user?.username}</span>
+                         </div>
+                       </div>
+                     );
+                   })
+                 : ""}
+             </div>
+           </ul>
+         ) : (
+           <></>
+         )}
+         {token ? (
+           <ul>
+             <li className="create-g">
+               <Link to="/new/gist">Create Gist</Link>
+             </li>
+             <li>
+               {" "}
+               <Link to="/chat">
+                 <i className="fas fa-comment-dots"></i>
+               </Link>
+             </li>
+             <li>
+               <i className="far fa-bell"></i>
+             </li>
+             <li>
+               <Link
+                 to={
+                   localStorage.getItem("profile_user_name")
+                     ? `/@/${localStorage.getItem("profile_user_name")}`
+                     : "/dash"
+                 }
+               >
+                 <img
+                   src={profileImage ?? defaultImage}
+                   className="profile-img"
+                   alt="profile"
+                 />
+               </Link>
+             </li>
+             <li>
+               <i
+                 className="fas fa-chevron-circle-down dropdown"
+                 onMouseOver={() => {
+                   document.querySelector(
+                     theme === "light"
+                       ? ".dropdown-content"
+                       : ".dropdown-content_dark"
+                   ).style.display = "block";
+                 }}
+               ></i>
+             </li>
+             <div
+               className={
+                 theme === "light"
+                   ? "dropdown-content"
+                   : "dropdown-content_dark"
+               }
+               style={{ right: 0 }}
+               onMouseLeave={() => {
+                 document.querySelector(
+                   theme === "light"
+                     ? ".dropdown-content"
+                     : ".dropdown-content_dark"
+                 ).style.display = "none";
+               }}
+             >
+               <p
+                 onClick={() => {
+                   history.push("/dash");
+                   document.querySelector(
+                     theme === "light"
+                       ? ".dropdown-content"
+                       : ".dropdown-content_dark"
+                   ).style.display = "none";
+                 }}
+               >
+                 <i className="fas fa-house-user"></i> <span>Dashboard</span>{" "}
+               </p>
+               <p
+                 onClick={() => {
+                   history.push("/settings");
+                   document.querySelector(
+                     theme === "light"
+                       ? ".dropdown-content"
+                       : ".dropdown-content_dark"
+                   ).style.display = "none";
+                 }}
+               >
+                 <i className="fas fa-sliders-h"></i> <span>Settings</span>
+               </p>
 
-        } else {
-            return;
-        }
-
-    }, [location, onlyInclude]);
-    useEffect(() => {
-        if (!token) return;
-            fetch(`${API_ENDPOINT}/user/name`, {
-                headers: {
-                    'Content-Type': 'application/json; charset=UTF-8',
-                    'Authorization': `Bearer ${localStorage.getItem("user_token")}`
-                },
-                method: "GET",
-            }).then(res => {
-                if (res.ok) return res.json();
-                return res.json().then(json => Promise.reject(json));
-            }
-            ).then(jsonRes => {
-                setProfileImage(jsonRes.message.profileImageUrl);
-                localStorage.setItem("profile_user_pic", jsonRes.message.profileImageUrl);
-                localStorage.setItem("profile_user_name", jsonRes.message.username);
-            }).catch(() => {
-                localStorage.removeItem("user_token");
-                history.replace("/login")
-                enqueueSnackbar("Unable to sync.", {
-                    variant: "error"
-                });
-            });
-    }, [token, enqueueSnackbar, history]);
-    const notAllowed = ["/", "/register", "/login", "/otp", "/onboard", "/chat"];
-
-    function logOut() {
-        localStorage.removeItem("user_token");
-        localStorage.setItem("new_user", "notreg");
-        localStorage.removeItem("profile_user_image");
-        history.push('/');
-    }
-    const handleChange = (event) => {
-        setValue(event.target.value);
-        const input = event.target.value;
-        if (event.target.value === "" || input.length < 4) {
-            return;
-        }
-        document.querySelector(".search_suggestions").style.display = "flex";
-        inputRef.current(input);
-    };
-    function handleKeyPress(e) {
-        if (e.key === "Enter") {
-            document.querySelector(".search_suggestions").style.display = "none";
-            setValue('');
-            history.replace(`/search?user=${value}`);
-        }
-    }
-    return <>
-        {
-            notAllowed.includes(location.pathname) || location.pathname.includes("chat") || location.pathname.includes("setup") ? '' :
-                <nav className={theme === "light" ? "navbar" : "navbar_dark"}>
-                    <ul>
-                        <li>
-                            <img className="comments" src={LogoPlaceholder} alt="icon" />
-                        </li>
-                        <li><Link to="/gists" className={location.pathname === "/gists" ? "active" : ""}>Public Gists</Link></li>
-                        {token ? <li><Link to="/feed" className={location.pathname === "/feed" ? "active" : ""}>Feed</Link></li> : <></>}
-                    </ul>
-                    {
-                        token ? <ul className="search-tab">
-                            <li><input type="search" name="search" id="search-nav" className="search-c" placeholder="Search" autoComplete="off" onClick={() => {
-                                document.querySelector(".search_suggestions").style.display = "flex";
-                            }} onChange={handleChange} onFocus={
-                                () => {
-                                    document.querySelector(".search_suggestions").style.display = "flex";
-                                }
-                            } value={value} onKeyPress={handleKeyPress} /></li>
-
-                            <div className="search_suggestions" onMouseLeave={() => {
-                                document.querySelector(".search_suggestions").style.display = "none";
-                            }}>
-                                {
-                                    users ? users.map((user, index) => {
-                                        return <div key={index}>
-
-
-                                            <div className="search_suggestion" onClick={() => {
-                                                setValue('');
-                                                history.replace(`/@/${user?.username}`);
-                                                document.querySelector(".search_suggestions").style.display = "none";
-                                            }}> <img alt="search_user" className="profile-img" src={user?.profileImageUrl ?? defaultImage} /> <span>{user?.username}</span></div>
-                                        </div>
-                                    }) : ''
-                                }
-
-                            </div>
-                        </ul> : <></>
-                    }
-                    {
-                        token ? <ul>
-                            <li className="create-g"><Link to="/new/gist">Create Gist</Link></li>
-                            <li > <Link to="/chat"><i className="fas fa-comment-dots"></i></Link></li>
-                            <li><i className="far fa-bell"></i></li>
-                            <li><Link to={localStorage.getItem("profile_user_name") ? `/@/${localStorage.getItem("profile_user_name")}` : "/dash"} ><img src={profileImage ?? defaultImage} className="profile-img" alt="profile" /></Link></li>
-                            <li><i className="fas fa-chevron-circle-down dropdown" onMouseOver={() => {
-
-                                document.querySelector(theme === "light" ? ".dropdown-content" : ".dropdown-content_dark").style.display = "block"
-                            }}
-                            ></i></li>
-                            <div className={theme === "light" ? "dropdown-content" : "dropdown-content_dark"} style={{ right: 0 }} onMouseLeave={() => {
-                                document.querySelector(theme === "light" ? ".dropdown-content" : ".dropdown-content_dark").style.display = "none"
-                            }} >
-                                <p onClick={() => {
-                                    history.push("/dash");
-                                    document.querySelector(theme === "light" ? ".dropdown-content" : ".dropdown-content_dark").style.display = "none";
-                                }}  ><i className="fas fa-house-user"  ></i> <span>Dashboard</span> </p>
-                                <p
-                                    onClick={() => {
-                                        history.push("/settings");
-                                        document.querySelector(theme === "light" ? ".dropdown-content" : ".dropdown-content_dark").style.display = "none";
-                                    }}
-
-                                ><i className="fas fa-sliders-h"></i>  <span>Settings</span></p>
-                                <p onClick={logOut} ><i className="fas fa-sign-out-alt"></i>  <span>Sign Out</span></p>
-                            </div>
-                        </ul> :
-                            (
-                                <></>
-                            )
-                    }
-                </nav>
-
-        }
-
-    </>
+               <p>
+                 <span style={{ marginRight: "1rem" }}>
+                   {theme === "light" ? "Dark Mode" : "Light Mode"}
+                 </span>{" "}
+                 <div className="switch">
+                   <input
+                     id="toggle1"
+                     onChange={handleTheme}
+                     className={
+                       theme === "light" ? "toggle-round1" : "toggle-round"
+                     }
+                     name="toggle"
+                     type="checkbox"
+                     ref={checkBox1}
+                   />
+                   <label htmlFor="toggle1"></label>
+                 </div>
+               </p>
+               <p onClick={logOut}>
+                 <i className="fas fa-sign-out-alt"></i> <span>Sign Out</span>
+               </p>
+             </div>
+           </ul>
+         ) : (
+           <></>
+         )}
+       </nav>
+     )}
+   </>
+ );
 }
 export default NavBar;
--- src/monacoEditor.js
+++ src/monacoEditor.js
@@ -8,16 +8,17 @@ import { IO } from "./utils/socket_stuff";
 import Renderer from "./components/renderer";
 import { debounce } from "@material-ui/core";
 import { useRef } from "react";
+import EditorPreloader from "./components/editor_preloader";
 
 function TextEditor(props) {
-  const { id } = useParams()
-  const [socket, setSocket] = useState()
+  const { id } = useParams();
+  const [socket, setSocket] = useState();
   const [theEditor, setTheEditor] = useState();
   const [code, setCode] = useState("");
   const [language, setLanguage] = useState("javascript");
   const [username, setUsername] = useState();
   const [loading, setLoading] = useState(true);
-  const [monaco, setMonaco] = useState()
+  const [monaco, setMonaco] = useState();
   const codeRef = useRef();
   const SAVE_INTERVAL_MS = 2000;
 
@@ -25,134 +26,150 @@ function TextEditor(props) {
     setTimeout(() => {
       setLoading(false);
     }, 5000);
-  }, [])
+  }, []);
   useEffect(() => {
     const interval = setInterval(() => {
       socket.emit("save-code", code);
-
-    }, SAVE_INTERVAL_MS)
+    }, SAVE_INTERVAL_MS);
 
     return () => {
-      clearInterval(interval)
-
-    }
-  }, [code, socket])
+      clearInterval(interval);
+    };
+  }, [code, socket]);
 
   useEffect(() => {
-    if (socket == null || theEditor == null) return
+    if (socket == null || theEditor == null) return;
     socket.on("receive-changes", ({ data, value, sender_id }) => {
       if (sender_id !== username) {
         setCode(value);
       }
-    })
-  }, [socket, theEditor, username])
-
+    });
+  }, [socket, theEditor, username]);
 
   useEffect(() => {
-
     const userToken = localStorage.getItem("user_token");
     fetch(`${API_ENDPOINT}/details`, {
       headers: {
-        'Content-Type': 'application/json; charset=UTF-8',
-        "Authorization": `Bearer ${userToken}`
+        "Content-Type": "application/json; charset=UTF-8",
+        Authorization: `Bearer ${userToken}`,
       },
       method: "GET",
-    }).then(res => res.json()
-    ).then(jsonRes => {
-      console.log(jsonRes, "jsonRes");
-      if (jsonRes.success) {
-        setUsername(jsonRes.username);
-      } else {
-        props.history.push("/not_allowed");
-      }
     })
+      .then((res) => res.json())
+      .then((jsonRes) => {
+        console.log(jsonRes, "jsonRes");
+        if (jsonRes.success) {
+          setUsername(jsonRes.username);
+        } else {
+          props.history.push("/not_allowed");
+        }
+      });
+  }, [props.history]);
 
-  }, [props.history])
-
-   useEffect(() => {
-     codeRef.current = debounce(sendCode, 700);
-      // eslint-disable-next-line
-   }, []);
   useEffect(() => {
-
-    if (socket == null || theEditor == null || username == null || monaco == null) return
+    codeRef.current = debounce(sendCode, 700);
+    // eslint-disable-next-line
+  }, []);
+  useEffect(() => {
+    if (
+      socket == null ||
+      theEditor == null ||
+      username == null ||
+      monaco == null
+    )
+      return;
     console.log(id);
     socket.emit("join-editor", { id, pubID: "none", type: "normal" });
     theEditor.updateOptions({ readOnly: true });
-    socket.once("load-code", code => {
+    socket.once("load-code", (code) => {
       if (username === code.username) {
         setCode(code.data);
         theEditor.updateOptions({ readOnly: false });
         setLanguage(code.language);
         monaco.editor.setModelLanguage(theEditor.getModel(), code.language);
         setLoading(false);
-
       } else {
         props.history.push("/not_allowed");
       }
-
     });
     socket.once("code_does_not_exist", () => {
       props.history.push("/code_not_found");
-    })
+    });
   }, [socket, theEditor, id, username, props.history, monaco]);
 
   useEffect(() => {
-    const s = IO(`${API_ENDPOINT}/editor1`)
-    setSocket(s)
+    const s = IO(`${API_ENDPOINT}/editor1`);
+    setSocket(s);
 
     return () => {
-      s.disconnect()
-    }
+      s.disconnect();
+    };
   }, []);
   function handleEditorDidMount(editor, monaco) {
     setTheEditor(editor);
     setMonaco(monaco);
   }
   function handleChange(value, event) {
-    codeRef.current(value, event,socket);
+    codeRef.current(value, event, socket);
   }
-   
-  function sendCode(value,event,socket) {
+
+  function sendCode(value, event, socket) {
     setCode(value);
-    socket.emit("send-changes", { data: event, value: value, randID: username });
+    socket.emit("send-changes", {
+      data: event,
+      value: value,
+      randID: username,
+    });
   }
   if (loading) {
-    return <div className="loading">
-      <span>Loading your  gist ... 👩🏻‍🦯</span>
-    </div >
+    return (
+      <div className="loading">
+        <EditorPreloader />
+      </div>
+    );
   } else {
     return (
       <>
-        
-        <div style={{marginTop:"1rem", height: "100%",overflow: "hidden",  display: "flex", flexDirection: "row", justifyContent: "space-evenly" }}>
-         
-          <div style={{marginTop:"1rem" , width:   language === "xml" || language === "html"? "50%":"100%" }} >
-            <Editor
-          value={code}
-            height="100vh"
-            
-          defaultLanguage={language}
-          options={{
-            minimap: {
-              enabled: true
-            },
-            fontSize: 18,
-            wordWrap: "on",
+        <div
+          style={{
+            marginTop: "1rem",
+            height: "100%",
+            overflow: "hidden",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-evenly",
           }}
-          onChange={handleChange}
-          onMount={handleEditorDidMount}
-          theme="vs-dark"
-          loading="Preparing your code... 🥳"
-        />
-           </div>
-         {
-          language === "xml" || language === "html" ? <Renderer code={ code}/>:''
-         }
-
-       </div>
+        >
+          <div
+            style={{
+              marginTop: "1rem",
+              width: language === "xml" || language === "html" ? "50%" : "100%",
+            }}
+          >
+            <Editor
+              value={code}
+              height="100vh"
+              defaultLanguage={language}
+              options={{
+                minimap: {
+                  enabled: true,
+                },
+                fontSize: 18,
+                wordWrap: "on",
+              }}
+              onChange={handleChange}
+              onMount={handleEditorDidMount}
+              theme="vs-dark"
+              loading={<></>}
+            />
+          </div>
+          {language === "xml" || language === "html" ? (
+            <Renderer code={code} />
+          ) : (
+            ""
+          )}
+        </div>
       </>
-
     );
   }
 }
