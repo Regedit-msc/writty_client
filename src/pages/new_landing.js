7c8fa1df8d2f8e5c77ce0e6fa05395b29d390adc
Regedit-msc <efusanyaae@gmail.com>
2022-01-13 06:41:53 +0100

    landing

--- src/pages/new_landing.js
+++ src/pages/new_landing.js
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 import LogoPlaceholder from "../images/logo.png";
 import CodeCollaboration from "../images/code_collaboration.svg";
+import HomeRect from "../images/home_rect.svg";
+import Private from "../images/private.svg";
+import Public from "../images/public.svg";
+import Feed from "../images/feed.svg";
+import Communicate from "../images/communicate.svg";
 import PublicGistsLanding from "../images/public_gists_landing.svg";
 import LandingSection1 from "../images/landing_section_1.svg";
 import LandingSection2 from "../images/landing_section_2.svg";
@@ -37,12 +42,13 @@ const LandingNavBarText = styled.p`
 const LandingNavBarOutlinedButton = styled.div`
   font-weight: bold;
   cursor: pointer;
+  padding: ${(props) => (props.padding ? props.padding : "0px")};
   height: 40px;
   margin: 5px;
   width: auto;
   border-radius: 30px;
   display: flex;
-  width: 150px;
+  width: ${(props) => (props.width ? props.width : "150px")};
   flex-direction: row;
   align-items: center;
   justify-content: center;
@@ -52,6 +58,7 @@ const LandingNavBarOutlinedButton = styled.div`
 const LandingNavBarButton = styled.div`
   font-weight: bold;
   cursor: pointer;
+  padding: ${(props) => (props.padding ? props.padding : "0px")};
   height: 40px;
   margin: 5px;
   width: auto;
@@ -72,7 +79,7 @@ const SectionLanding = styled.div`
 `;
 const GetStartedButton = styled.div`
   cursor: pointer;
-
+  font-weight: bold;
   display: flex;
   color: white;
   flex-direction: row;
@@ -84,6 +91,23 @@ const GetStartedButton = styled.div`
   height: 10px;
   border-radius: 10px;
 `;
+const ColouredButton = styled.div`
+  cursor: pointer;
+  margin-bottom: 20px;
+  font-weight: bold;
+  display: flex;
+  color: white;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+  width: ${(props) => props.width};
+  color: ${(props) => props.color};
+  background-color: ${(props) => props.bgColor};
+  height: 10px;
+  border-radius: 10px;
+`;
+
 const NewLanding = () => {
   return (
     <>
@@ -121,15 +145,24 @@ const NewLanding = () => {
             marginLeft: "150px",
           }}
         >
-          <h1 style={{ fontWeight: "bold" }}>What is Live-Gists?</h1>
-          <p style={{ fontWeight: "bold", textAlign: "left" }}>
+          <h1 style={{ fontWeight: "bold", fontSize: "30px" }}>
+            What is Live-Gists?
+          </h1>
+          <p
+            style={{
+              fontWeight: "bold",
+              textAlign: "left",
+
+              lineHeight: "30px",
+            }}
+          >
             Live Gists is a code sharing platform where developers can
             collaborate and write code simultaneously.
           </p>
           <GetStartedButton color="black">Get Started</GetStartedButton>
         </div>
         <div style={{ display: "flex", flexDirection: "column" }}>
-          <img style={{ width: "700px" }} src={LandingSection1} />
+          <img style={{ width: "700px" }} src={LandingSection1} alt="" />
           <div
             style={{
               position: "absolute",
@@ -154,7 +187,6 @@ const NewLanding = () => {
             top: "10%",
             left: "0",
             right: "0",
-
             marginLeft: "auto",
             marginRight: "auto",
             width: "70%",
@@ -171,12 +203,16 @@ const NewLanding = () => {
             }}
           >
             <div>
-              <h1 style={{ color: "#546270" }}>See code snippets from</h1>
+              <h1 style={{ color: "#546270", fontSize: "30px" }}>
+                See code snippets from
+              </h1>
 
-              <h1>Other Developers.</h1>
+              <h1 style={{ fontSize: "30px" }}>Other Developers.</h1>
             </div>
 
-            <div style={{ width: "400px", fontWeight: "bold" }}>
+            <div
+              style={{ width: "400px", fontWeight: "bold", lineHeight: "30px" }}
+            >
               Users’ codes, called gists can be found on the public gists page.
               Here, codes written by other developers which are made public can
               be seen.
@@ -201,12 +237,247 @@ const NewLanding = () => {
 
       <SectionLanding
         style={{
-          position: "relative",
           marginTop: "100px",
-          backgroundColor: "#7A99C8",
-          height: "500px",
+          backgroundColor: "rgba(122,153,200,0.52)",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "flex-start",
         }}
-      ></SectionLanding>
+      >
+        <h1 style={{ fontWeight: "bold", fontSize: "35px", color: "#3E5475" }}>
+          Easy Customization
+        </h1>
+        <h1>For All Users</h1>
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <ColouredButton bgColor="white" color="black">
+              Light Mode
+            </ColouredButton>
+            <img style={{ width: "600px" }} src={LightMode} alt="landing" />
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              padding: "30px",
+            }}
+          >
+            <ColouredButton bgColor="black" color="#678DC9">
+              Dark Mode
+            </ColouredButton>
+            <img style={{ width: "600px" }} src={DarkMode} alt="landing" />
+          </div>
+        </div>
+      </SectionLanding>
+      <SectionLanding
+        style={{
+          marginTop: "100px",
+          marginBottom: "100px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+            padding: "10px",
+            width: "50%",
+          }}
+        >
+          <div>
+            <h1 style={{ color: "#546270", fontSize: "30px" }}>
+              See what other
+            </h1>
+
+            <h1 style={{ fontSize: "30px" }}>developers are up to.</h1>
+          </div>
+
+          <div
+            style={{ width: "300px", fontWeight: "bold", lineHeight: "30px" }}
+          >
+            The feed section allows you to see activities from other developers
+            and also recommend others to follow and posts to engage with.
+          </div>
+        </div>
+        <img
+          style={{ width: "900px", objectFit: "contain" }}
+          src={Feed}
+          alt="landing"
+        />
+      </SectionLanding>
+      <SectionLanding
+        style={{
+          marginTop: "100px",
+          backgroundColor: "#24252D",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "flex-start",
+          paddingTop: "100px",
+          paddingBottom: "100px",
+          marginBottom: "100px",
+        }}
+      >
+        <h1 style={{ fontWeight: "bold", fontSize: "30px", color: "white" }}>
+          Communicate with
+        </h1>
+        <h1 style={{ fontWeight: "bold", fontSize: "30px", color: "#8599AC" }}>
+          other developers
+        </h1>
+        <p
+          style={{
+            fontWeight: "bold",
+            color: "white",
+            textAlign: "center",
+            width: "500px",
+            lineHeight: "30px",
+          }}
+        >
+          Connect with other developers to share important information and
+          discuss about topics of intrest.
+        </p>
+        <img
+          style={{ width: "1000px", marginTop: "40px" }}
+          src={Communicate}
+          alt="landing"
+        />
+      </SectionLanding>
+      <SectionLanding
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          paddingTop: "100px",
+          justifyContent: "space-between",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+            padding: "10px",
+            width: "50%",
+          }}
+        >
+          <div>
+            <h1 style={{ color: "#547061", fontSize: "30px" }}>
+              Make your gists
+            </h1>
+
+            <h1 style={{ fontSize: "35px", fontWeight: "bold" }}>
+              public or private.
+            </h1>
+          </div>
+
+          <div
+            style={{ width: "300px", fontWeight: "bold", lineHeight: "30px" }}
+          >
+            You can decide to make the codes you write, called gists private or
+            public.
+          </div>
+        </div>
+        <br />
+        <br />
+        <br />
+        <div style={{ position: "relative" }}>
+          <div
+            style={{
+              position: "absolute",
+              top: "30%",
+              left: "50%",
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+              transform: "translate(-50%, -50%)",
+              width: "500px",
+            }}
+          >
+            <img
+              style={{ width: "200px", objectFit: "contain" }}
+              src={Private}
+              alt="landing"
+            />
+
+            <img
+              style={{ width: "200px", objectFit: "contain" }}
+              src={Public}
+              alt="landing"
+            />
+          </div>
+          <img
+            style={{ width: "100%", objectFit: "contain" }}
+            src={HomeRect}
+            alt="landing"
+          />
+        </div>
+      </SectionLanding>
+      <SectionLanding
+        style={{
+          padding: "100px",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "space-between",
+            padding: "10px",
+            width: "100%",
+          }}
+        >
+          <div>
+            <h1 style={{ fontWeight: "bold", fontSize: "35px" }}>
+              Ready to try
+            </h1>
+
+            <h1
+              style={{ color: "#546270", fontSize: "35px", fontWeight: "bold" }}
+            >
+              Live- Gists for yourself?
+            </h1>
+          </div>
+          <br /> <br /> <br /> <br />
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+              width: "500px",
+            }}
+          >
+            <LandingNavBarButton color="black" padding="10px">
+              Join the dev team?
+            </LandingNavBarButton>
+            <LandingNavBarOutlinedButton
+              color={"black"}
+              width="auto"
+              padding="10px"
+            >
+              Sign up for early access
+            </LandingNavBarOutlinedButton>
+          </div>
+        </div>
+      </SectionLanding>
     </>
   );
 };
